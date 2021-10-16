c0a1a5f14cd0a4700aa6e3ad0ab3eec8bd421137
Tranvansu2001 <Tranvansu2001@gmail.com>
2021-10-16 12:28:09 +0700

    cmsn

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 cmsn = () => {
   alert("From LuNgu with luv!");
-  alert("Chúc Mừng Sinh Nhật Em Hằng <3");
+  alert("Chúc Mừng Sinh Nhật Em Hằng ❤️");
   const SelectBtn = document.querySelector(".picture");
   SelectBtn.style.display = "flex";
 
-  const image = document.querySelector(".image");
-  image.style.height = "700px";
+  const image1 = document.querySelector(".image1");
+  image1.style.height = "700px";
+  const image2 = document.querySelector(".image2");
+  image2.style.height = "700px";
+  const image3 = document.querySelector(".image3");
+  image3.style.height = "700px";
+  const image4 = document.querySelector(".image4");
+  image4.style.height = "700px";
+  const image5 = document.querySelector(".image5");
+  image5.style.height = "500px";
+  const text = document.querySelector(".text");
+  text.style.display = "block";
 };
