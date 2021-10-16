14947981e6bee72ca47a231f2392c8d120800504
Tranvansu2001 <Tranvansu2001@gmail.com>
2021-10-16 09:16:52 +0700

    cmsn

---
+++ main.js
@@ -0,0 +1,9 @@
+cmsn = () => {
+  alert("From LuNgu with luv!");
+  alert("Chúc Mừng Sinh Nhật Em Hằng <3");
+  const SelectBtn = document.querySelector(".picture");
+  SelectBtn.style.display = "flex";
+
+  const image = document.querySelector(".image");
+  image.style.height = "700px";
+};
